eaa93f9655011751f921f5b56a8f44b365e2f0bf
Oleg Kachalov <KachalovOI@kamaz.ru>
2020-11-06 15:49:57 +0300

    update er image

--- server/server/database/Queries.py
+++ server/server/database/Queries.py
@@ -1,4 +1,4 @@
 class Queries:
     SAVE_DATA = 'INSERT INTO test.test_table(id, temp, hum, count) VALUES (MD5(NOW(3)), %s, %s, %s);'
 
-    TEST = 'SELECT X(point), Y(point) FROM addresses WHERE id = 3;'
+    TEST = 'SELECT X(coordinates), Y(coordinates) FROM addresses WHERE id = 1;'
