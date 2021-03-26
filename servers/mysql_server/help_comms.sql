85a20abb4d71acde7ab26301a7028c25ebb0aba1
Oleg Kachalov <okachalov@icloud.com>
2021-03-26 22:39:47 +0300

    update mysql database

--- servers/mysql_server/help_comms.sql
+++ servers/mysql_server/help_comms.sql
@@ -3,3 +3,6 @@ SET GLOBAL event_scheduler = ON;
 -- DROP EVENT RowTTL;
 SHOW EVENTS;
 SHOW PROCESSLIST;
+
+UPDATE IcicleSpy3.addresses SET IcicleSpy3.addresses.point = POINT(20, 20) WHERE id = 3;
+SELECT X(point), Y(point) FROM addresses WHERE id = 3;
