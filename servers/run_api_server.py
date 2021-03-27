1867ba76f97ec5103ad7b1f8aa368069462e2719
Oleg Kachalov <okachalov@icloud.com>
2021-03-27 17:23:50 +0300

    basic api server

---
+++ servers/run_api_server.py
@@ -0,0 +1,46 @@
+import json
+import argparse
+
+import uvicorn
+
+from fastapi import Depends, FastAPI, HTTPException, status, Header
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
+from utils.database.DBConnection import DBConnection
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', type=int, default=8000)
+    parser.add_argument('--config', type=str, required=True)
+
+    args = parser.parse_args()
+    print(args)
+
+    cfg = json.loads(open(args.config, 'r').read())
+
+    app = FastAPI()
+    security = HTTPBasic()
+    db = DBConnection(cfg['mysql']['host'], cfg['mysql']['port'], cfg['mysql']['user'], cfg['mysql']['password'])
+
+    def http_auth(credentials: HTTPBasicCredentials = Depends(security)):
+        if not (db.auth_check(credentials.username, credentials.password)):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect email or password",
+                headers={"WWW-Authenticate": "Basic"},
+            )
+
+        return True
+
+
+    @app.get("/get_data")
+    def get_data(_=Depends(http_auth), id: int = Header(None), fields: str = Header(None)):
+        if id > 0 and fields != '':
+            fields = json.loads(fields)
+            data = db.get_data_by_id(id, fields)
+            return {'status': True, 'data': data}
+        else:
+            return {'status': False, 'data': []}
+
+
+    uvicorn.run(app, port=args.port)
