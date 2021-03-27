d5e720fdd549bef2f9c642b708e0d8ff151a1aa3
Oleg Kachalov <okachalov@icloud.com>
2021-03-27 15:00:39 +0300

    fix POINT coordinates

--- servers/mysql_server/ExampleData.sql
+++ servers/mysql_server/ExampleData.sql
@@ -1,43 +1,45 @@
 -- RUNCATE TABLE `IcicleSpy3`.`addresses`;
 
+--  Additional information: POINT(Y, X) -> (longitude, latitude)
+
 --  Taganskaya
 INSERT INTO `IcicleSpy3`.`addresses` (`coordinates`, `country`, `region_state`, `city`, `street`, `building`, `index`)
-VALUES (POINT(55.73981389011555, 37.665907144546516), 'Russia', 'Moscow', 'Moscow', 'Taganskaya', '25-27', '1');
+VALUES (POINT(37.665907144546516, 55.73981389011555), 'Russia', 'Moscow', 'Moscow', 'Taganskaya', '25-27', '1');
 
 INSERT INTO `IcicleSpy3`.`addresses` (`coordinates`, `country`, `region_state`, `city`, `street`, `building`, `index`) 
-VALUES (POINT(55.73969007364334, 37.66753387451172), 'Russia', 'Moscow', 'Moscow', 'Taganskaya', '29c1', '1');
+VALUES (POINT(37.66753387451172, 55.73969007364334), 'Russia', 'Moscow', 'Moscow', 'Taganskaya', '29c1', '1');
 
 INSERT INTO `IcicleSpy3`.`addresses` (`coordinates`, `country`, `region_state`, `city`, `street`, `building`, `index`)
-VALUES (POINT(55.73939110054219, 37.66586422920228), 'Russia', 'Moscow', 'Moscow', 'Taganskaya', '34c1', '1');
+VALUES (POINT(37.66586422920228, 55.73939110054219), 'Russia', 'Moscow', 'Moscow', 'Taganskaya', '34c1', '1');
 
 INSERT INTO `IcicleSpy3`.`addresses` (`coordinates`, `country`, `region_state`, `city`, `street`, `building`, `index`)
-VALUES (POINT(55.73934580138767, 37.66638457775117), 'Russia', 'Moscow', 'Moscow', 'Taganskaya', '36c1', '1');
+VALUES (POINT(37.66638457775117, 55.73934580138767), 'Russia', 'Moscow', 'Moscow', 'Taganskaya', '36c1', '1');
 
 INSERT INTO `IcicleSpy3`.`addresses` (`coordinates`, `country`, `region_state`, `city`, `street`, `building`, `index`)
-VALUES (POINT(55.739312581974275, 37.66676545143128), 'Russia', 'Moscow', 'Moscow', 'Taganskaya', '38c1', '1');
+VALUES (POINT(37.66676545143128, 55.739312581974275), 'Russia', 'Moscow', 'Moscow', 'Taganskaya', '38c1', '1');
 
 
 -- Prospekt Mira
 INSERT INTO `IcicleSpy3`.`addresses` (`coordinates`, `country`, `region_state`, `city`, `street`, `building`, `index`)
-VALUES (POINT(55.774696359636295, 37.631961107254035), 'Russia', 'Moscow', 'Moscow', 'Prospekt Mira', '5', '2');
+VALUES (POINT(37.631961107254035, 55.774696359636295), 'Russia', 'Moscow', 'Moscow', 'Prospekt Mira', '5', '2');
 
 INSERT INTO `IcicleSpy3`.`addresses` (`coordinates`, `country`, `region_state`, `city`, `street`, `building`, `index`)
-VALUES (POINT(55.77502825125138, 37.631939649581916), 'Russia', 'Moscow', 'Moscow', 'Prospekt Mira', '7', '2');
+VALUES (POINT(37.631939649581916, 55.77502825125138), 'Russia', 'Moscow', 'Moscow', 'Prospekt Mira', '7', '2');
 
 INSERT INTO `IcicleSpy3`.`addresses` (`coordinates`, `country`, `region_state`, `city`, `street`, `building`, `index`)
-VALUES (POINT(55.77549892905928, 37.6318645477295), 'Russia', 'Moscow', 'Moscow', 'Prospekt Mira', '9', '2');
+VALUES (POINT(37.6318645477295, 55.77549892905928), 'Russia', 'Moscow', 'Moscow', 'Prospekt Mira', '9', '2');
 
 INSERT INTO `IcicleSpy3`.`addresses` (`coordinates`, `country`, `region_state`, `city`, `street`, `building`, `index`)
-VALUES (POINT(55.77581271110671, 37.63138175010682), 'Russia', 'Moscow', 'Moscow', 'Prospekt Mira', '11', '2');
+VALUES (POINT(37.63138175010682, 55.77581271110671), 'Russia', 'Moscow', 'Moscow', 'Prospekt Mira', '11', '2');
 
 INSERT INTO `IcicleSpy3`.`addresses` (`coordinates`, `country`, `region_state`, `city`, `street`, `building`, `index`)
-VALUES (POINT(55.775354105723814, 37.63283014297486), 'Russia', 'Moscow', 'Moscow', 'Prospekt Mira', '16c1', '2');
+VALUES (POINT(37.63283014297486, 55.775354105723814), 'Russia', 'Moscow', 'Moscow', 'Prospekt Mira', '16c1', '2');
 
 INSERT INTO `IcicleSpy3`.`addresses` (`coordinates`, `country`, `region_state`, `city`, `street`, `building`, `index`)
-VALUES (POINT(55.77566185466846, 37.63266921043397), 'Russia', 'Moscow', 'Moscow', 'Prospekt Mira', '16c2', '2');
+VALUES (POINT(37.63266921043397, 55.77566185466846), 'Russia', 'Moscow', 'Moscow', 'Prospekt Mira', '16c2', '2');
 
 INSERT INTO `IcicleSpy3`.`addresses` (`coordinates`, `country`, `region_state`, `city`, `street`, `building`, `index`)
-VALUES (POINT(55.77611442223143, 37.63265848159791), 'Russia', 'Moscow', 'Moscow', 'Prospekt Mira', '20k1', '2');
+VALUES (POINT(37.63265848159791, 55.77611442223143), 'Russia', 'Moscow', 'Moscow', 'Prospekt Mira', '20k1', '2');
 
 -- Add devices
 INSERT INTO `IcicleSpy3`.`devices` (`token`, `addresses_id`) VALUES ('DEV1', '3');
