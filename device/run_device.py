a658403fce68c85571fd6d1e98340150e91e162e
Oleg Kachalov <okachalov@icloud.com>
2021-04-09 22:10:32 +0300

    first alpha prototype

--- device/run_device.py
+++ device/run_device.py
@@ -1,6 +1,8 @@
 import json
 import argparse
 
+import time
+
 from RTSPCap import RTSPCap
 from YoloObjectDetection import YoloObjectDetection
 from classes.IcicleSpyPackage import IcicleSpyPackage
@@ -13,3 +15,16 @@ if __name__ == '__main__':
 
     cfg = json.loads(open(args.config, 'r').read())
     print(cfg)
+
+    cameras = []
+    for cam in cfg['cameras']:
+        cameras.append(RTSPCap(source=cam['source'], raw_source=True))
+    for cam in cameras:
+        cam.start()
+
+    while True:
+        for cam in cameras:
+            frame = cam.read()
+            print(f'{cam.source} - ok')
+
+        time.sleep(60 * cfg['d_time'])
