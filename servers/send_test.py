4ccc86a92acf6bb1977678140e20ae16fe4c08db
Oleg Kachalov <okachalov@icloud.com>
2021-04-11 22:52:00 +0300

    test file

--- servers/send_test.py
+++ servers/send_test.py
@@ -1,11 +1,14 @@
 import requests
 import pickle
 
-test = {'test': 1}
-data = pickle.dumps(test)
-files = {'file': data}
+from classes.IcicleSpyPackage import IcicleSpyPackage
 
-r = requests.post('http://127.0.0.1:8000/save_data_from_device/dev_test', files=files)
+test = IcicleSpyPackage()
+# test = {'test': 1}
+data = pickle.dumps(test)
+files = {'data': data}
+
+r = requests.post('http://127.0.0.1:8000/save_data_from_device/DEV1', files=files)
 
 print(r)
 print(r.status_code)
