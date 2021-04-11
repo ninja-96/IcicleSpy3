d2d14a3544c9192b662f042c86489a643d6eea10
Oleg Kachalov <okachalov@icloud.com>
2021-04-11 21:36:04 +0300

    add program to test server receive opportunity

--- servers/send_test.py
+++ servers/send_test.py
@@ -5,7 +5,7 @@ test = {'test': 1}
 data = pickle.dumps(test)
 files = {'file': data}
 
-r = requests.post('http://127.0.0.1:8000/uploadfile', files=files)
+r = requests.post('http://127.0.0.1:8000/save_data_from_device/dev_test', files=files)
 
 print(r)
 print(r.status_code)
