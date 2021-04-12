597bd2408bd83d1fe4d31b60edff99f7f3077108
Oleg Kachalov <okachalov@icloud.com>
2021-04-12 21:53:17 +0300

    add http basic auth for devices

--- servers/run_api_server.py
+++ servers/run_api_server.py
@@ -7,7 +7,8 @@ import uvicorn
 from fastapi import Depends, FastAPI, HTTPException, status, Header, File
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
-from utils.database.DBConnection import DBConnection
+from utils import DeviceAuth
+from database.DBConnection import DBConnection
 
 from classes.IcicleSpyPackage import IcicleSpyPackage
 
@@ -22,11 +23,14 @@ if __name__ == '__main__':
     cfg = json.loads(open(args.config, 'r').read())
 
     app = FastAPI()
-    security = HTTPBasic()
+    user_security = HTTPBasic()
+
+    device_auth = DeviceAuth(cfg['device_security']['login'], cfg['device_security']['password'])
+    device_security = HTTPBasic()
     db = DBConnection(cfg['mysql']['host'], cfg['mysql']['port'], cfg['mysql']['user'], cfg['mysql']['password'])
 
 
-    def user_http_auth(credentials: HTTPBasicCredentials = Depends(security)):
+    def user_http_auth(credentials: HTTPBasicCredentials = Depends(user_security)):
         if not (db.user_auth_check(credentials.username, credentials.password)):
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
@@ -37,13 +41,13 @@ if __name__ == '__main__':
         return True
 
 
-    def device_http_auth(credentials: HTTPBasicCredentials = Depends(security)):
-        # if not (db.user_auth_check(credentials.username, credentials.password)):
-        #     raise HTTPException(
-        #         status_code=status.HTTP_401_UNAUTHORIZED,
-        #         detail="Incorrect email or password",
-        #         headers={"WWW-Authenticate": "Basic"},
-        #     )
+    def device_http_auth(credentials: HTTPBasicCredentials = Depends(device_security)):
+        if not (device_auth.check(credentials.username, credentials.password)):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Auth failed",
+                headers={"WWW-Authenticate": "Basic"},
+            )
 
         return True
 
@@ -51,7 +55,7 @@ if __name__ == '__main__':
     @app.get("/get_data")
     def get_data(_=Depends(user_http_auth), id: int = Header(None), fields: str = Header(None)):
         if id > 0 and fields != '':
-            fields = json.loads(fields)
+            fields: list = json.loads(fields)
             data = db.get_data_by_id(id, fields)
             return {'status': True, 'data': data}
         else:
@@ -60,10 +64,10 @@ if __name__ == '__main__':
 
     @app.post("/save_data_from_device")
     def save_data_from_device(_=Depends(device_http_auth), data: bytes = File(...)):
-        isp = pickle.loads(data)
+        isp: IcicleSpyPackage = pickle.loads(data)
 
         if type(isp) == IcicleSpyPackage:
-            if not db.device_auth_check(isp.device_id):
+            if not (db.device_in_db(isp.device_token) and db.camera_in_db(isp.camera_token)):
                 raise HTTPException(
                     status_code=status.HTTP_403_FORBIDDEN,
                     detail="Forbidden"
