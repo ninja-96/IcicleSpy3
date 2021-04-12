62beb4bb7b985158dde1a9d3610f824abb42f284
Oleg Kachalov <okachalov@icloud.com>
2021-04-12 21:52:54 +0300

    add new queries

--- servers/database/Queries.py
+++ servers/database/Queries.py
@@ -1,6 +1,11 @@
 class Queries:
     USER_AUTH_CHECK = "SELECT EXISTS(SELECT 1 FROM IcicleSpy3.users WHERE (IcicleSpy3.users.login = %s AND IcicleSpy3.users.password = %s))"
-    DEVICE_AUTH_CHECK = "SELECT EXISTS(SELECT 1 FROM IcicleSpy3.devices AS T WHERE T.token = %s)"
+
+    DEVICE_IN_DB = "SELECT EXISTS(SELECT 1 FROM IcicleSpy3.devices AS T WHERE T.token = %s)"
+    CAMERA_IN_DB = "SELECT EXISTS(SELECT 1 FROM IcicleSpy3.cameras AS T WHERE T.token = %s)"
+
+    DEVICE_ID_BY_TOKEN = "SELECT T.id FROM IcicleSpy3.devices AS T WHERE T.token = %s"
+    CAMERA_ID_BY_TOKEN = "SELECT T.id FROM IcicleSpy3.cameras AS T WHERE T.token = %s"
 
     ASSOCIATE_CHECK = "SELECT IF (((SELECT D.id FROM IcicleSpy3.devices AS D WHERE D.token = %s) = (SELECT C.devices_id FROM IcicleSpy3.cameras AS C WHERE C.token = %s)), 1, 0)"
     DEVICE_RECORD_ID_EXISTS = "SELECT T.id FROM IcicleSpy3.devices_data AS T WHERE (T.devices_id = %s AND T.cameras_id = %s)"
