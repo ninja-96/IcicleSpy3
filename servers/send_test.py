10dfef64ec33f091f39103cbf4cdbd0cfff72d11
Oleg Kachalov <okachalov@icloud.com>
2021-04-12 21:53:22 +0300

    fix

--- servers/send_test.py
+++ servers/send_test.py
@@ -1,14 +1,20 @@
 import requests
+from requests.auth import HTTPBasicAuth
 import pickle
+
+import datetime
 
 from classes.IcicleSpyPackage import IcicleSpyPackage
 
 test = IcicleSpyPackage()
+test.time = datetime.datetime.now()
+test.device_token = 'DEV1'
+test.camera_token = 'CAM1'
 # test = {'test': 1}
 data = pickle.dumps(test)
 files = {'data': data}
 
-r = requests.post('http://127.0.0.1:8000/save_data_from_device/DEV1', files=files)
+r = requests.post('http://127.0.0.1:8000/save_data_from_device', files=files, auth=HTTPBasicAuth('login', 'password'))
 
 print(r)
 print(r.status_code)
