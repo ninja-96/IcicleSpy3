5df26ada72a9b9582b90b16ac154c0fe2ee74792
ninja-96 <okachalov@icloud.com>
2021-04-18 19:07:13 +0300

    update device program

--- device/run_device.py
+++ device/run_device.py
@@ -2,6 +2,11 @@ import json
 import argparse
 
 import time
+import random
+from datetime import datetime
+
+import cv2
+import numpy as np
 
 from RTSPCap import RTSPCap
 from YoloObjectDetection import YoloObjectDetection
@@ -24,7 +29,17 @@ if __name__ == '__main__':
 
     while True:
         for cam in cameras:
+            isp: IcicleSpyPackage = IcicleSpyPackage()
+
             frame = cam.read()
-            print(f'{cam.source} - ok')
+
+            isp.time = datetime.now()
+            isp.temperature = random.randint(-20, 20) + random.random()
+            isp.air_humidity = random.randint(0, 99) + random.random()
+            isp.count = random.randint(0, 20)
+            isp.bbox = []
+            isp.img = cv2.imencode('.jpg', frame)[1]
+            isp.device_token = cfg['token']
+            isp.camera_token = cam.token
 
         time.sleep(60 * cfg['d_time'])
