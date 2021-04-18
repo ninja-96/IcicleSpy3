de085e99a5f4fe46bf73af8e8d1bb56c604c3fb2
ninja-96 <okachalov@icloud.com>
2021-04-18 19:07:18 +0300

    fix

--- api_server/send_test.py
+++ api_server/send_test.py
@@ -6,16 +6,21 @@ import datetime
 
 from classes.IcicleSpyPackage import IcicleSpyPackage
 
-test = IcicleSpyPackage()
-test.time = datetime.datetime.now()
-test.device_token = 'DEV1'
-test.camera_token = 'CAM1'
-# test = {'test': 1}
-data = pickle.dumps(test)
-files = {'data': data}
+header = {'token': 'DEV1'}
 
-r = requests.post('http://127.0.0.1:8000/save_data_from_device', files=files, auth=HTTPBasicAuth('login', 'password'))
-
+r = requests.get('http://127.0.0.1:8000/get_icicle_count', auth=HTTPBasicAuth('login', 'password'), headers=header)
 print(r)
-print(r.status_code)
 print(r.text)
+
+# test = IcicleSpyPackage()
+# test.time = datetime.datetime.now()
+# test.device_token = 'DEV1'
+# test.camera_token = 'CAM1'
+# data = pickle.dumps(test)
+# files = {'data': data}
+#
+# r = requests.post('http://127.0.0.1:8000/save_data_from_device', files=files, auth=HTTPBasicAuth('login', 'password'))
+#
+# print(r)
+# print(r.status_code)
+# print(r.text)
