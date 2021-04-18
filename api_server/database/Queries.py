4256f90122530bdb5b942f21fcc7d95aeba1d46e
ninja-96 <okachalov@icloud.com>
2021-04-18 21:26:09 +0300

    comment one query

--- api_server/database/Queries.py
+++ api_server/database/Queries.py
@@ -7,7 +7,7 @@ class Queries:
     DEVICE_ID_BY_TOKEN = "SELECT T.id FROM IcicleSpy3.devices AS T WHERE T.token = %s"
     CAMERA_ID_BY_TOKEN = "SELECT T.id FROM IcicleSpy3.cameras AS T WHERE T.token = %s"
 
-    ASSOCIATE_CHECK = "SELECT IF (((SELECT D.id FROM IcicleSpy3.devices AS D WHERE D.token = %s) = (SELECT C.devices_id FROM IcicleSpy3.cameras AS C WHERE C.token = %s)), 1, 0)"
+    # ASSOCIATE_CHECK = "SELECT IF (((SELECT D.id FROM IcicleSpy3.devices AS D WHERE D.token = %s) = (SELECT C.devices_id FROM IcicleSpy3.cameras AS C WHERE C.token = %s)), 1, 0)"
     DEVICE_RECORD_ID_EXISTS = "SELECT T.id FROM IcicleSpy3.devices_data AS T WHERE (T.devices_id = %s AND T.cameras_id = %s)"
     SAVE_DEVICE_PACKAGE = "INSERT INTO IcicleSpy3.devices_data (time, temperature, air_humidity, count, bbox, img, devices_id, cameras_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
     UPDATE_DEVICE_RECORD = "UPDATE IcicleSpy3.devices_data SET time = %s, temperature = %s, air_humidity = %s, count = %s, bbox = %s, img = %s WHERE (id = %s AND devices_id = %s AND cameras_id = %s)"
